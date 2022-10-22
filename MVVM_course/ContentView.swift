6b545f34edd27bc6a32864a45b83cae6622411b4
GArifullinII <ildaridrisovich@gmail.com>
2022-10-22 19:44:08 +0400

    Initial Commit

---
+++ MVVM_course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_course
+//
+//  Created by Ildar Garifullin on 22/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_course/MVVM_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_courseApp.swift
+//  MVVM_course
+//
+//  Created by Ildar Garifullin on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
